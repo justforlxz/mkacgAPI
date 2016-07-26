bc40f6ef7de7a27f6522b8d62db00935de9fb057
kirigayakazushin <kirigaya@mkacg.com>
2016-07-27 07:54:41 +0800

    测试API

--- modules/backdata.go
+++ modules/backdata.go
@@ -1,16 +1,42 @@
 package modules
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
 
-func News(cs ...string) {
+	"github.com/martini-contrib/render"
+)
+
+type NewsSource struct {
+	Title  string
+	Source string
+	Img    string
+	Next   string
+	Url    string
+}
+
+func News(r render.Render, w http.ResponseWriter, request *http.Request) {
 	//规定参数顺序
 	/*
 	   1.type 只能是json
 	   2.page
 	*/
-	if cs[0] == "json" {
-		News_index("s")
+	if request.FormValue("type") == "json" {
+		if len(request.FormValue("p")) == 0 {
+			T1, T2 := News_index("http://acg.17173.com")
+			r.JSON(200, map[string]interface{}{"News": T1, "Page": T2})
+		} else if request.FormValue("p") == "begin" || request.FormValue("p") == "end" {
+			T1, T2 := News_index("http://acg.17173.com")
+			r.JSON(200, map[string]interface{}{"News": T1, "Page": T2})
+		} else {
+			var Page = "http://acg.17173.com/index_" + request.FormValue("p") + ".shtml"
+			T1, T2 := News_index(Page)
+			r.JSON(200, map[string]interface{}{"News": T1, "Page": T2})
+		}
+	} else {
+		r.Text(200, "请输入正确的参数")
 	}
+
 }
 func Bangumi(cs ...string) {
 	for _, num := range cs {
--- web.go
+++ web.go
@@ -1,11 +1,21 @@
 package main
 
-import "github.com/go-martini/martini"
+import (
+	"github.com/go-martini/martini"
+	"github.com/kirigayakazushin/mkacgAPI/modules"
+	"github.com/martini-contrib/render"
+)
 
 func main() {
 	m := martini.Classic()
-	m.Get("/", func() string {
-		return "Hello world!"
-	})
+	m.Use(render.Renderer(render.Options{
+		Directory:  "templates",
+		Layout:     "layout",
+		Extensions: []string{".tmpl", ".html"},
+		Charset:    "UTF-8",
+	}))
+
+	m.Get("/news", modules.News)
+	m.Get("/bangumi", modules.News)
 	m.Run()
 }
