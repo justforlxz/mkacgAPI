f8afcf818b265d76ed4f918e78f010befa1951c3
kirigayakazushin <kirigaya@mkacg.com>
2016-07-26 08:25:40 +0800

    first

--- web.go
+++ web.go
@@ -1,22 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-	"os"
-	"runtime"
-)
+import "github.com/go-martini/martini"
 
 func main() {
-	http.HandleFunc("/", hello)
-	bind := fmt.Sprintf("%s:%s", os.Getenv("OPENSHIFT_GO_IP"), os.Getenv("OPENSHIFT_GO_PORT"))
-	fmt.Printf("listening on %s...", bind)
-	err := http.ListenAndServe(bind, nil)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func hello(res http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(res, "hello, world from %s", runtime.Version())
+	m := martini.Classic()
+	m.Get("/", func() string {
+		return "Hello world!"
+	})
+	m.Run()
 }
